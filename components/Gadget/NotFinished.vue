4946c3c3621eee82039f27bd43ac8f2e1c66e0af
Yung-Kuo <yungkuo429@gmail.com>
2025-03-04 01:32:54 +0800

    minor fixes

--- components/Gadget/NotFinished.vue
+++ components/Gadget/NotFinished.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="fixed left-1/2 top-6 z-40 flex h-12 -translate-x-1/2 items-center justify-center rounded-lg bg-red-600 px-10"
+    class="fixed bottom-6 left-1/2 z-40 flex h-12 -translate-x-1/2 items-center justify-center rounded-lg bg-red-600 px-10"
   >
     <span class="whitespace-nowrap text-xl font-bold text-neutral-300"
       >Still in the works...</span
