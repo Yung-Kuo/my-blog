ce42d360267f9f4b223e99760fb24d30487875e2
Yung-Kuo <yungkuo429@gmail.com>
2025-06-11 10:25:21 +0800

    gadget default

--- components/Gadget/Github.vue
+++ components/Gadget/Github.vue
@@ -1,9 +1,7 @@
 <template>
   <NuxtLink to="https://github.com/Yung-Kuo" target="_blank">
-    <div
-      class="z-30 flex h-12 w-min items-center justify-center rounded-lg bg-neutral-400 bg-opacity-40 px-4 text-neutral-800 backdrop-blur-sm transition-all hover:invert dark:bg-neutral-700 dark:bg-opacity-40 dark:text-neutral-300"
-    >
+    <GadgetDefault class="w-min">
       <span class="text-xl font-bold">Github</span>
-    </div>
+    </GadgetDefault>
   </NuxtLink>
 </template>
--- components/Gadget/Home.vue
+++ components/Gadget/Home.vue
@@ -1,9 +1,7 @@
 <template>
   <NuxtLink to="/">
-    <div
-      class="fixed z-30 flex h-12 w-12 items-center justify-center rounded-lg bg-neutral-400 bg-opacity-40 text-neutral-800 backdrop-blur-sm transition-all hover:invert dark:bg-neutral-700 dark:bg-opacity-40 dark:text-neutral-300"
-    >
+    <GadgetDefault class="h-12 w-12 !px-0">
       <IconsHome />
-    </div>
+    </GadgetDefault>
   </NuxtLink>
 </template>
