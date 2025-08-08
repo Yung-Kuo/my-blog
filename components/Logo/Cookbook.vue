c2286ff8a1840d174557fa2cff3b9e71017bfb1a
Yung-Kuo <yungkuo429@gmail.com>
2025-08-09 04:33:43 +0800

    cookbook logo adjust

--- components/Logo/Cookbook.vue
+++ components/Logo/Cookbook.vue
@@ -3,17 +3,17 @@
   <div class="flex w-full justify-center">
     <div class="group grid grid-cols-1 grid-rows-1">
       <h1
-        class="z-20 col-start-1 row-start-1 -translate-x-1 -translate-y-1 text-5xl font-bold text-yellow-300 transition-all group-hover:-translate-x-10 group-hover:-translate-y-10 sm:text-6xl"
+        class="z-20 col-start-1 row-start-1 -translate-x-1 -translate-y-1 text-5xl font-bold text-yellow-400 transition-all group-hover:-translate-x-10 group-hover:-translate-y-10 dark:text-yellow-300 sm:text-6xl"
       >
         Cookbook
       </h1>
       <h1
-        class="z-10 col-start-1 row-start-1 text-5xl font-bold text-orange-500 transition-all sm:text-6xl"
+        class="z-10 col-start-1 row-start-1 text-5xl font-bold text-orange-600 transition-all dark:text-orange-500 sm:text-6xl"
       >
         Cookbook
       </h1>
       <h1
-        class="col-start-1 row-start-1 translate-x-1 translate-y-1 text-5xl font-bold text-red-700 transition-all group-hover:translate-x-10 group-hover:translate-y-10 sm:text-6xl"
+        class="col-start-1 row-start-1 translate-x-1 translate-y-1 text-5xl font-bold text-red-800 transition-all group-hover:translate-x-10 group-hover:translate-y-10 dark:text-red-700 sm:text-6xl"
       >
         Cookbook
       </h1>
