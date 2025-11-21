8f6cd25846d2025291a62326ea5e39667d24d842
Yung-Kuo <yungkuo429@gmail.com>
2025-11-21 11:05:44 +0800

    add GoToTop gadget

---
+++ components/Gadget/GoToTop.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+<template>
+  <div class="fixed bottom-6 right-6 z-30 rounded-full">
+    <a href="#goToTop">
+      <GadgetDefault class="h-12 w-12 !rounded-full active:scale-90">
+        <span class="text-2xl font-bold">&#8593;</span>
+      </GadgetDefault>
+    </a>
+  </div>
+</template>
